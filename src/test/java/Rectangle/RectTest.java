3f118e7e4be32996531a9f1612261215db5eb4ec
mondzik <mondzik777@gmail.com>
2017-01-07 17:12:06 +0100

    wszystkie metody są, nie wiem czy działają

---
+++ src/test/java/Rectangle/RectTest.java
@@ -0,0 +1,29 @@
+package Rectangle;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Mondi on 07.01.2017.
+ */
+public class RectTest {
+    @org.junit.Test
+    public void distanceBeetweenRectCenters() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void crossing() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void contains() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void commonFields() throws Exception {
+
+    }
+
+}
